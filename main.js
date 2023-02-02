ce23bfdd8cc683160f0517ed48311a30945cdeb8
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-02-02 19:17:34 +0000

    fixed all the errors in the code

--- main.js
+++ main.js
@@ -6,39 +6,52 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const imgArray = ['images/pic1.jpg', 'images/pic2.jpgg', 'iimages/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
+const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 
 /* Declaring the alternative text for each image file */
 
 const altText = {
-    'images/pic1.jpg' : "An eye,",
-    'images/pic2.jpg' : "Sea Shell",
-    'images/pic3.jpg' : "Purple and White flowers",
-    'images/pic4.jpg' : "Egyptian Images",
-    'images/pic5.jpg' : "Butterfly on a leaf"
+  'pic1.jpg': "An eye,",
+  'pic2.jpg': "Sea Shell",
+  'pic3.jpg': "Purple and White flowers",
+  'pic4.jpg': "Egyptian Images",
+  'pic5.jpg': "Butterfly on a leaf"
 }
 
 /* Looping through images */
 
 for (const image of imgArray) {
-    const newImage = document.createAttribute('img');
-newImage.setAttribute('src', imgArray);
-newImage.setAttribute('alt', altText);
-thumbBar.appendChild(newImage);
-newImage.addEventlistner ('click', () ==> {
-displayedImage.src = imgArray
-displayedImage.alt = altText
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`)
+  newImage.setAttribute('alt', altText[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src
+    displayedImage.alt = e.target.alt
+  }
+  );
 }
-);
-}
+// Here is how to do it with a regular loop
+
+// for (let i = 0; i < imgArray.length; i++) {
+//   const newImage = document.createElement('img');
+//   newImage.setAttribute('src', `images/${imgArray[i]}`)
+//   newImage.setAttribute('alt', altText[i]);
+//   thumbBar.appendChild(newImage);
+//   newImage.addEventListener('click', e => {
+//     displayedImage.src = e.target.src
+//     displayedImage.alt = e.target.alt
+//   }
+//   );
+// }
 
 /* Wiring up the Darken/Lighten button */
 
-btn.addEventListener("click", updateOverlay);
+
 function updateOverlay(event) {
   const curBtn = event.target;
-  const btnClass = curBtn.setAttribute("class");
+  const btnClass = curBtn.getAttribute("class");
   if (btnClass === "dark") {
     curBtn.setAttribute("class", "light");
     curBtn.textContent = "lighten";
@@ -49,3 +62,4 @@ function updateOverlay(event) {
     overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
 }
+btn.addEventListener("click", updateOverlay);
